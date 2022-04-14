628274d12ea4c9e36b4602bb9f696f97912bee80
TasukuY <alohaty81@gmail.com>
2022-04-13 23:38:28 -0600

    TaskListViewController part done

--- Task/Controllers/ViewControllers/TaskDetailViewController.swift
+++ Task/Controllers/ViewControllers/TaskDetailViewController.swift
@@ -14,28 +14,31 @@ class TaskDetailViewController: UIViewController {
     @IBOutlet weak var taskNotesTextView: UITextView!
     @IBOutlet weak var taskDueDatePicker: UIDatePicker!
     
+    //MARK: - Properties
+    var task: Task?
+    
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
     
     //MARK: - IBActions
     @IBAction func saveButtonTapped(_ sender: Any) {
+        if let task = task {
+            //existing task cell tapped
+            guard let name = taskNameTextField.text, let notes = taskNotesTextView.text else { return }
+            let dueDate = taskDueDatePicker.date
+            TaskController.shared.update(task: task, newName: name, newNotes: notes, newDueDate: dueDate)
+        }else{
+            //add button tapped
+            guard let name = taskNameTextField.text, let notes = taskNotesTextView.text else { return }
+            let dueDate = taskDueDatePicker.date
+            TaskController.shared.createTaskWith(name: name, notes: notes, dueDate: dueDate)
+        }
+        
+        navigationController?.popViewController(animated: true)//to go back to the task lists view
     }
     
     @IBAction func dueDatePickerDateChanged(_ sender: Any) {
     }
-    
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
